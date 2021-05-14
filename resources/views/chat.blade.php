7f80774e560048597cd82058cc6e21e5798e35e3
Jhoel Guerra <guerrajhoel10@gmail.com>
2021-05-13 22:07:41 -0500

    Start

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat Zendy</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <h1>Aqui estara el chat</h1>
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
