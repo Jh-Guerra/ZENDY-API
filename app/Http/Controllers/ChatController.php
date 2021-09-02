fbdf6d6d6955819c7e5c539b1ca2ba7a80abf0e3
Jhoel Guerra <guerrajhoel10@gmail.com>
2021-09-02 01:22:23 -0500

    RECUPERAR LOS PARTICIPANTES DEL CHAT

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -18,6 +18,14 @@ class ChatController extends Controller
             return response()->json(['error' => 'Chat no encontrado'], 400);
         }
 
+        $users = User::whereIn("id", $chat->get("idUser"))->get()->keyBy('id');
+        $receiver = User::whereIn("id", $chat->get("idReceiver"))->get()->keyBy('id');
+        $companies = Company::whereIn("id", $chat->get("allUsers"))->get()->keyBy('id');
+
+        $chat->user = $chat->idUser ? $users[$chat->idUser] : null;
+        $chat->receiver = $chat->idReceiver ? $receiver[$chat->idReceiver] : null;
+        $chat->company = $chat->idCompany ? $companies[$chat->idCompany] : null;
+
         return response()->json(compact('chat'),201);
     }
 
