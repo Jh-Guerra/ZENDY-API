cc9f63337a16ed2924f9f467e059001fc39c4efc
DESKTOP-A0Q70AK\cchin <0332171006@unjfsc.edu.pe>
2021-09-25 02:11:57 -0500

    Revert "REEMPLAZAR LA SECCIÓN “HISTORIAL DE CHATS” POR UNA DINÁMICA"
    This reverts commit 1dd7d6aa7716b0166c6a243ba1ee3ae0b060225c.

--- routes/api.php
+++ routes/api.php
@@ -48,7 +48,6 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     Route::prefix('chats')->group(function () {
         Route::get('/find/{id}', 'App\Http\Controllers\ChatController@find');
         Route::get('/active-list', 'App\Http\Controllers\ChatController@listActive');
-        Route::get('/not-active-list', 'App\Http\Controllers\ChatController@listNotActive');
         Route::delete('/delete/{id}', 'App\Http\Controllers\ChatController@delete');
     });
 
