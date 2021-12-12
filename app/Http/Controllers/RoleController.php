f8f04a1b66cf8eb1238765f082b2e7e8ec5307a3
Kevin Valencia <kevinvalenciadiaz18@gmail.com>
2021-12-11 21:31:04 -0500

    remove

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -1,13 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Company;
 
 use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    public function list(){
-        return Role::where("status", true)->get();
+    public function list(Request $request){
+        $idCompany = $request->has("idCompany") ? $request->get("idCompany") : null;
+        $company = Company::where("id", $idCompany)->where("deleted", false)->first();
+        if($company->isHelpDesk){
+            return Role::where("status", true)->where("id","!=", "4")->get();
+        } else {
+            return Role::where("status", true)->get();            
+        }
     }
 }
