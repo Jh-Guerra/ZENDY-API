9bc47284af0a5b63a34cf0eb403b855e49ac6518
Edu Estula <edu.estula@ext.blaze.me>
2022-01-12 23:11:20 -0500

    V3 fix: Validate if company exists for the user logged

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -10,11 +10,16 @@ class RoleController extends Controller
 {
     public function list(Request $request){
         $idCompany = $request->has("idCompany") ? $request->get("idCompany") : null;
-        $company = Company::where("id", $idCompany)->where("deleted", false)->first();
-        if($company->isHelpDesk){
+        if($idCompany){
+            $company = Company::where("id", $idCompany)->where("deleted", false)->first();
+            if($company->isHelpDesk){
+                return Role::where("status", true)->get();
+            } else {
+                return Role::where("status", true)->where("id","!=", "4")->get();
+            }
+        }else{
             return Role::where("status", true)->get();
-        } else {
-            return Role::where("status", true)->where("id","!=", "4")->get();
         }
+
     }
 }
